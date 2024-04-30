c3878ffc6f735362d116abd09778dd95380e483e
atroopa <atroopa@gmail.com>
2024-04-30 03:15:29 -0700

    maghalat be sakhti tamam shod

--- index.php
+++ index.php
@@ -182,7 +182,7 @@ if ($index1_post->have_posts()) {
               </div>
             </div>
             <?PHP if (has_post_thumbnail()) { ?>
-            <img src="<?PHP the_post_thumbnail_url(); ?>" alt="<?PHP the_title(); ?>"class="w-full h-80"  />
+            <img src="<?PHP the_post_thumbnail_url(); ?>" alt="<?PHP the_title(); ?>" class="w-full h-80"  />
             <?PHP } ?>
           </div>
           <?PHP
@@ -195,65 +195,68 @@ if ($index1_post->have_posts()) {
       </div>
     </div>
   </div>
+  <!-- ################################################################################################################ -->
   <section class="dark:bg-gray-100 dark:text-gray-800">
     <div class="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
-      <a rel="noopener noreferrer" href="#" class="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:bg-gray-50">
-        <img src="https://source.unsplash.com/random/480x360" alt="" class="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500">
-        <div class="p-6 space-y-2 lg:col-span-5">
-          <h3 class="text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">Noster tincidunt reprimique ad pro</h3>
-          <span class="text-xs dark:text-gray-600">February 19, 2021</span>
-          <p>Ei delenit sensibus liberavisse pri. Quod suscipit no nam. Est in graece fuisset, eos affert putent doctus id.</p>
+      <!-- ----------------------------------------------------------------------------- -->
+      <?PHP
+            $index6_post = new WP_Query(array(
+              'post_type' => 'post',
+              'posts_per_page' => '1',
+              'offset'=> '6',
+              'post_status' => 'publish',
+              'category_name' => 'green-house-blog'
+            ));
+
+            if ($index6_post->have_posts()) {
+              while ($index6_post->have_posts()) {
+                $index6_post->the_post();
+            ?>
+      <a  href="<?PHP the_permalink(); ?>"  class="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:bg-gray-50">
+      <?PHP if (has_post_thumbnail()) { ?>
+      <img src="<?PHP the_post_thumbnail_url(); ?>" alt="<?PHP the_title(); ?>" class="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500">
+      <?PHP } ?>
+      <div class="p-6 space-y-2 lg:col-span-5">
+          <h3 class="text-xl font-semibold sm:text-3xl group-hover:underline group-focus:underline"><?PHP the_title(); ?></h3>
+          <span class="text-xs dark:text-gray-600"><?PHP echo get_the_date(); ?></span>
+          <div><?php echo wp_trim_words(get_the_excerpt(), 72); ?></div>
         </div>
       </a>
+      <?PHP
+            }
+          }
+            ?>
+      <!-- ------------------------------------حالت ششم --------------------------------------------- -->
       <div class="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-        <a rel="noopener noreferrer" href="#" class="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-50">
-          <img role="presentation" class="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://source.unsplash.com/random/480x360?1">
-          <div class="p-6 space-y-2">
-            <h3 class="text-2xl font-semibold group-hover:underline group-focus:underline">In usu laoreet repudiare legendos</h3>
-            <span class="text-xs dark:text-gray-600">January 21, 2021</span>
-            <p>Mei ex aliquid eleifend forensibus, quo ad dicta apeirian neglegentur, ex has tantas percipit perfecto. At per tempor albucius perfecto, ei probatus consulatu patrioque mea, ei vocent delicata indoctum pri.</p>
+        <!-- ------------------------------------------------------- -->
+        <?PHP
+            $index7_post = new WP_Query(array(
+              'post_type' => 'post',
+              'posts_per_page' => '6',
+              'offset'=> '7',
+              'post_status' => 'publish',
+              'category_name' => 'green-house-blog'
+            ));
+
+            if ($index7_post->have_posts()) {
+              while ($index7_post->have_posts()) {
+                $index7_post->the_post();
+            ?>
+        <a href="<?PHP the_permalink(); ?>"  class="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-50">
+        <?PHP if (has_post_thumbnail()) { ?> 
+        <img  class="object-cover w-full rounded h-44 dark:bg-gray-500" src="<?PHP the_post_thumbnail_url(); ?>" alt="<?PHP the_title(); ?>">
+        <?PHP } ?>
+        <div class="p-6 space-y-2">
+            <h3 class="text-2xl font-semibold group-hover:underline group-focus:underline"><?PHP the_title(); ?></h3>
+            <span class="text-xs dark:text-gray-600"><?PHP echo get_the_date(); ?></span>
+            <div><?php echo wp_trim_words(get_the_excerpt(), 72); ?></div>
           </div>
         </a>
-        <a rel="noopener noreferrer" href="#" class="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-50">
-          <img role="presentation" class="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://source.unsplash.com/random/480x360?2">
-          <div class="p-6 space-y-2">
-            <h3 class="text-2xl font-semibold group-hover:underline group-focus:underline">In usu laoreet repudiare legendos</h3>
-            <span class="text-xs dark:text-gray-600">January 22, 2021</span>
-            <p>Mei ex aliquid eleifend forensibus, quo ad dicta apeirian neglegentur, ex has tantas percipit perfecto. At per tempor albucius perfecto, ei probatus consulatu patrioque mea, ei vocent delicata indoctum pri.</p>
-          </div>
-        </a>
-        <a rel="noopener noreferrer" href="#" class="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-50">
-          <img role="presentation" class="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://source.unsplash.com/random/480x360?3">
-          <div class="p-6 space-y-2">
-            <h3 class="text-2xl font-semibold group-hover:underline group-focus:underline">In usu laoreet repudiare legendos</h3>
-            <span class="text-xs dark:text-gray-600">January 23, 2021</span>
-            <p>Mei ex aliquid eleifend forensibus, quo ad dicta apeirian neglegentur, ex has tantas percipit perfecto. At per tempor albucius perfecto, ei probatus consulatu patrioque mea, ei vocent delicata indoctum pri.</p>
-          </div>
-        </a>
-        <a rel="noopener noreferrer" href="#" class="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-50 hidden sm:block">
-          <img role="presentation" class="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://source.unsplash.com/random/480x360?4">
-          <div class="p-6 space-y-2">
-            <h3 class="text-2xl font-semibold group-hover:underline group-focus:underline">In usu laoreet repudiare legendos</h3>
-            <span class="text-xs dark:text-gray-600">January 24, 2021</span>
-            <p>Mei ex aliquid eleifend forensibus, quo ad dicta apeirian neglegentur, ex has tantas percipit perfecto. At per tempor albucius perfecto, ei probatus consulatu patrioque mea, ei vocent delicata indoctum pri.</p>
-          </div>
-        </a>
-        <a rel="noopener noreferrer" href="#" class="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-50 hidden sm:block">
-          <img role="presentation" class="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://source.unsplash.com/random/480x360?5">
-          <div class="p-6 space-y-2">
-            <h3 class="text-2xl font-semibold group-hover:underline group-focus:underline">In usu laoreet repudiare legendos</h3>
-            <span class="text-xs dark:text-gray-600">January 25, 2021</span>
-            <p>Mei ex aliquid eleifend forensibus, quo ad dicta apeirian neglegentur, ex has tantas percipit perfecto. At per tempor albucius perfecto, ei probatus consulatu patrioque mea, ei vocent delicata indoctum pri.</p>
-          </div>
-        </a>
-        <a rel="noopener noreferrer" href="#" class="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-50 hidden sm:block">
-          <img role="presentation" class="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://source.unsplash.com/random/480x360?6">
-          <div class="p-6 space-y-2">
-            <h3 class="text-2xl font-semibold group-hover:underline group-focus:underline">In usu laoreet repudiare legendos</h3>
-            <span class="text-xs dark:text-gray-600">January 26, 2021</span>
-            <p>Mei ex aliquid eleifend forensibus, quo ad dicta apeirian neglegentur, ex has tantas percipit perfecto. At per tempor albucius perfecto, ei probatus consulatu patrioque mea, ei vocent delicata indoctum pri.</p>
-          </div>
-        </a>
+        <?PHP
+            }
+          }
+            ?>
+        <!-- -------------------------------------حالت هفتم  ------------------------------ -->
       </div>
       <div class="flex justify-center">
         <button type="button" class="px-6 py-3 text-sm rounded-md hover:underline dark:bg-gray-50 dark:text-gray-600">Load more posts...</button>
