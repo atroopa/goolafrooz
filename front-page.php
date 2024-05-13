c47fca3375dbe09b693d52d2fdcc716ca478038f
atroopa <atroopa@gmail.com>
2024-05-13 05:32:40 -0700

    changed dark scaled image to webp

--- front-page.php
+++ front-page.php
@@ -177,7 +177,7 @@
   </div>
   <!-- <div>------------------نمونه کار-----------------</div> -->
   <div class="w-full relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
-    <img src="https://golafrooz.com/wp-content/uploads/2024/04/dark-scaled.jpg" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center">
+    <img src="<?php echo get_template_directory_uri().'/img/another/dark-scaled.webp' ?>" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center">
     <div class="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl" aria-hidden="true">
       <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
     </div>
