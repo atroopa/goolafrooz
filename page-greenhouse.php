bbc551b45be22fd037cba8245d6bbc486a75a49b
atroopa <atroopa@gmail.com>
2024-05-12 22:31:10 -0700

    start green house page

---
+++ page-greenhouse.php
@@ -0,0 +1,18 @@
+<?PHP // Template Name: greenhouse
+    get_header();
+?>
+
+
+<div class="flex flex-col justify-center items-center h-screen">
+    <div class="bg-cover bg-no-repeat w-full"
+        style="background-image: url('https://images.unsplash.com/photo-1530053969600-caed2596d242');">
+        <h1 class="py-10 font-black text-8xl md:text-9xl bg-gray-100 mix-blend-lighten uppercase text-center">
+            گلخانه سازی  <br>
+            گل افروز <br>
+    
+        </h1>
+    </div>
+</div>
+
+
+<?PHP get_footer(); ?>
