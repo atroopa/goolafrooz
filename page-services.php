f2ae7abb97843e7a0076ce5d2cbf4b192a571eb0
atroopa <atroopa@gmail.com>
2024-05-13 03:09:52 -0700

    services page step 2 ok

--- page-services.php
+++ page-services.php
@@ -47,5 +47,66 @@ get_header();
     </div>
 
 </div>
-
+<!-- ---------------------------------------------------------- -->
+<!-- inspired by tailwindcss.com -->
+<ul class="grid grid-cols-1 xl:grid-cols-3 gap-y-10 gap-x-6 items-start p-8">
+    <li class="relative flex flex-col sm:flex-row xl:flex-col items-start">
+        <div class="order-1 sm:ml-6 xl:ml-0">
+            <h3 class="mb-1 text-slate-900 font-semibold">
+                <span class="mb-1 block text-sm leading-6 text-indigo-500">معماری سازه ها</span>Completely unstyled, fully
+                accessible UI components
+            </h3>
+            <div class="prose prose-slate prose-sm text-slate-600">
+                <p>Completely unstyled, fully accessible UI components, designed to integrate beautifully with Tailwind
+                    CSS.</p>
+            </div><a
+                class="group inline-flex items-center h-9 rounded-full text-sm font-semibold whitespace-nowrap px-3 focus:outline-none focus:ring-2 bg-slate-100 text-slate-700 hover:bg-slate-200 hover:text-slate-900 focus:ring-slate-500 mt-6"
+                href="">Learn
+                more<span class="sr-only">, Completely unstyled, fully accessible UI components</span>
+                <svg class="overflow-visible ml-3 text-slate-300 group-hover:text-slate-400"
+                    width="3" height="6" viewBox="0 0 3 6" fill="none" stroke="currentColor" stroke-width="2"
+                    stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M0 0L3 3L0 6"></path>
+                </svg></a>
+        </div>
+        <img src="https://tailwindcss.com/_next/static/media/headlessui@75.c1d50bc1.jpg" alt="" class="mb-6 shadow-md rounded-lg bg-slate-50 w-full sm:w-[17rem] sm:mb-0 xl:mb-6 xl:w-full" width="1216" height="640">
+    </li>
+    <li class="relative flex flex-col sm:flex-row xl:flex-col items-start">
+        <div class="order-1 sm:ml-6 xl:ml-0">
+            <h3 class="mb-1 text-slate-900 font-semibold">
+                <span class="mb-1 block text-sm leading-6 text-purple-500">قرارداد</span>آشنایی با قراردادهای گلخانه سازی گل افروز
+            </h3>
+            <div class="prose prose-slate prose-sm text-slate-600">
+                <p>تمامی قراردادها مطابق با نیاز شما طراحی شده و در اختیار شما قرار داده می شود . در این نوع قرارداد ها  انواع مسائل مهم حقوقی قید می شود تا شما مشتریان محترم بتوانید مطمئن تر در زیمینه ساخت گلخانه اقدام کنید </p>
+            </div><a
+                class="group inline-flex items-center h-9 rounded-full text-sm font-semibold whitespace-nowrap px-3 focus:outline-none focus:ring-2 bg-slate-100 text-slate-700 hover:bg-slate-200 hover:text-slate-900 focus:ring-slate-500 mt-6"
+                href="">بیشتر بخوانید<span class="sr-only">آشنایی با انواع قرارداد های شکرت گلخانه سازی گل افروز</span>
+                <svg class="overflow-visible ml-3 text-slate-300 group-hover:text-slate-400"
+                    width="3" height="6" viewBox="0 0 3 6" fill="none" stroke="currentColor" stroke-width="2"
+                    stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M0 0L3 3L0 6"></path>
+                </svg></a>
+        </div>
+        <img src="https://tailwindcss.com/_next/static/media/heroicons@75.4a558f35.jpg" alt="" class="mb-6 shadow-md rounded-lg bg-slate-50 w-full sm:w-[17rem] sm:mb-0 xl:mb-6 xl:w-full" width="1216" height="640">
+    </li>
+    <li class="relative flex flex-col sm:flex-row xl:flex-col items-start">
+        <div class="order-1 sm:ml-6 xl:ml-0">
+            <h3 class="mb-1 text-slate-900 font-semibold">
+                <span class="mb-1 block text-sm leading-6 text-cyan-500">کاتالوگ</span>
+                کاتالوگ های شرکت گلخانه سازی گل افروز
+            </h3>
+            <div class="prose prose-slate prose-sm text-slate-600">
+                <p>با دیدن کاتالوگ های شرکت گلخانه سازی گل افروز ضمن اینکه با انواع طرح های گلخانه سازی آشنا می شوید می توانید نمونه کارهای ما را همراه با توضیحات بخوانید و از محل های تاسیس آن ها آگاه شوید</p>
+            </div><a
+                class="group inline-flex items-center h-9 rounded-full text-sm font-semibold whitespace-nowrap px-3 focus:outline-none focus:ring-2 bg-slate-100 text-slate-700 hover:bg-slate-200 hover:text-slate-900 focus:ring-slate-500 mt-6"
+                href="">بیشتر بخوانید<span class="sr-only">, Seamless SVG background patterns by the makers of Tailwind CSS.</span>
+                <svg class="overflow-visible ml-3 text-slate-300 group-hover:text-slate-400"
+                    width="3" height="6" viewBox="0 0 3 6" fill="none" stroke="currentColor" stroke-width="2"
+                    stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M0 0L3 3L0 6"></path>
+                </svg></a>
+        </div>
+        <img src="https://tailwindcss.com/_next/static/media/heropatterns@75.82a09697.jpg" alt="" class="mb-6 shadow-md rounded-lg bg-slate-50 w-full sm:w-[17rem] sm:mb-0 xl:mb-6 xl:w-full" width="1216" height="640">
+    </li>
+</ul>
 <?PHP get_footer();  ?>
