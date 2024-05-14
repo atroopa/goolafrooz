8cdf46323c08321d92fc30e475c74c87bd09e785
atroopa <atroopa@gmail.com>
2024-05-14 03:58:23 -0700

    favicon fixed

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+  <link rel="shortcut icon" href= "<?php echo get_template_directory_uri() ?>/img/logo/golafrooz.png" />
   <script src="https://cdn.tailwindcss.com"></script>
   <script>
     tailwind.config = {
