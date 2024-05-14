e184259878dd9e9b64a56165fd98cfea03a6bd2f
atroopa <atroopa@gmail.com>
2024-05-13 23:49:45 -0700

    gallery page images fixed

--- page-greenhouse.php
+++ page-greenhouse.php
@@ -5,7 +5,7 @@ get_header();
 <div>
 
     <div class="flex flex-col justify-center items-center h-400 drop-shadow-lg">
-        <div class=" bg-cover bg-no-repeat w-full" style="background-image: url('https://images.unsplash.com/photo-1530053969600-caed2596d242');">
+        <div class=" bg-cover bg-no-repeat w-full" style="background-image: url(<?php echo get_template_directory_uri().'/img/another/gallexy.webp' ?>);">
             <h1 class=" py-10 font-black text-7xl md:text-8xl bg-gray-100 mix-blend-lighten uppercase text-center">
                 گلخانه سازی <br>
                 گل افروز <br>
